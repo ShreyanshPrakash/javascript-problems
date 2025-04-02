0a0955f944a58c3342623d9f9a8a76c424541b4a
Shreyansh Prakash <shreyanshprakash@gentlemachine-2.local>
2025-04-02 22:12:52 +0530

    [feature/polyfills] : Added unshit polyfill method to update the same array

--- Polyfills/Arrays/unshift.js
+++ Polyfills/Arrays/unshift.js
@@ -11,14 +11,33 @@ function unshiftCustom(...rest){
     return array;
 }
 
+// updating the same array by shifting the elements
+function unshiftMutation(...rest){
+    const newItems = rest;
+    const newItemsCount = newItems.length;
+    const len = this.length;
+
+    for(let i = len - 1; i >= 0; i--){
+        this[i + newItemsCount] = this[i];
+        this[i] = null;
+    }
+
+    for(let i = 0; i < newItemsCount; i++){
+        this[i] = rest[i];
+    }
+
+    return this;
+    
+}
+
 
 const sampleArray = [1,2,3,4];
 
 let result = unshiftBasic(sampleArray, 4);
 
-console.log(result);
+// console.log(result);
 
-Array.prototype.unshift = unshiftCustom;
+Array.prototype.unshift = unshiftMutation;
 result = sampleArray.unshift(4,5);
 console.log(result);
 
