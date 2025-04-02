cfc3780797fd9abdac61cf843cbcc7f5e6a7207f
Shreyansh Prakash <shreyanshprakash@gentlemachine-2.local>
2025-04-02 22:32:33 +0530

    [feature/polyfills] : Added shift array polyfill

---
+++ Polyfills/Arrays/shift.js
@@ -0,0 +1,36 @@
+
+
+
+
+const shiftBasic = (array) => {
+    const len = array.length;
+    for(let i = 0; i < len; i++){
+        array[i] = array[i + 1];
+    }
+    array.length = len - 1;
+    return array;
+}
+
+
+function shiftCustom(){
+    const array = this;
+    const len = array.length;
+
+    for(let i = 0; i < len; i++){
+        array[i] = array[i + 1];
+    }
+
+    array.length = len - 1;
+    return array;
+}
+
+
+
+const sampleArray = [1,2,3,4];
+
+let result = shiftBasic(sampleArray);
+console.log(result);
+
+Array.prototype.shift = shiftCustom;
+result = sampleArray.shift();
+console.log(result);
