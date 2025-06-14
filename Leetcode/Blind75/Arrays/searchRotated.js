8f13af7f0461db5ac701392ac840d622abb32a12
Shreyansh Prakash <shreyanshprakash@gentlemachine-2.local>
2025-06-14 16:05:31 +0530

    [feature/polyfills] : Added Search algorithms

---
+++ Leetcode/Blind75/Arrays/searchRotated.js
@@ -0,0 +1,35 @@
+
+/*
+33. Search in Rotated Sorted Array
+
+There is an integer array nums sorted in ascending order (with distinct values).
+
+Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+
+Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+You must write an algorithm with O(log n) runtime complexity.
+*/
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+
+// This solution works only when all the fields have the all natural numbers
+var search = function(nums, target) {
+
+    const len = nums.length;
+
+    const valueAtTarget = nums[target];
+    const valueAtRotatedTarget = nums[valueAtTarget];
+
+    if(valueAtRotatedTarget === target){
+        return valueAtTarget;
+    }
+
+    return -1;
+
+};
